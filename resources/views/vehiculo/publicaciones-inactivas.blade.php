46cb5790cbf81cb8c11a2464f4b563205b46237b
Domingo Ilarreta <akimxtreme.dj@gmail.com>
2015-09-16 13:21:20 -0430

    Cambios modulo vehiculos

--- resources/views/vehiculo/publicaciones-inactivas.blade.php
+++ resources/views/vehiculo/publicaciones-inactivas.blade.php
@@ -11,7 +11,7 @@
 		
         <section class="content">
           <div class="row">
-            <div class="col-xs-10 col-md-8 col-md-offset-2">             
+            <div class="col-xs-10 col-md-10 col-md-offset-1">             
 
             @if(Session::has('message'))
 
